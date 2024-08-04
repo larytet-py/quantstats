1d1c78f1f96f775459ae20ed00597982fcab7477
larytet <>
2024-08-04 12:29:49 +0300

    Add test report

--- test.py
+++ test.py
@@ -1,18 +1,27 @@
 %matplotlib inline
 import pandas as pd
 import quantstats as qs
+import argparse
+
+parser = argparse.ArgumentParser(description='Calculate Sharpe Ratio from trades CSV file.')
+parser.add_argument('filename', type=str, help='The path to the trades CSV file')
+
+# Parse the arguments
+args = parser.parse_args()
+
 
 # Extend pandas functionality with metrics, etc.
 qs.extend_pandas()
 
+
 # Read the CSV file into a DataFrame
-trades = pd.read_csv('trades.csv', parse_dates=['End Time', 'Next Start Time'])
+trades = pd.read_csv(args.filename, parse_dates=['End Time', 'Next Start Time'])
 
 # Set the 'End Time' as the index
 trades.set_index('End Time', inplace=True)
 
 # Resample to daily frequency and forward-fill missing values
-daily_equity = trades['Equity'].resample('D').ffill()
+daily_equity = trades['Balance'].resample('D').ffill()
 
 # Calculate daily returns
 daily_returns = daily_equity.pct_change().dropna()
