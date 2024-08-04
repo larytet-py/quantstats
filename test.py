74bf6281a38b606e443ce3f4f7b710ec47e02995
larytet <>
2024-08-04 12:48:16 +0300

    Add test report

--- test.py
+++ test.py
@@ -2,24 +2,17 @@
 # pip3 install -r requirements.txt
 # pip3 install notebook
 # ~/.local/bin/jupyter-notebook
-%matplotlib inline
 import pandas as pd
 import quantstats as qs
-import argparse
 
-parser = argparse.ArgumentParser(description='Calculate Sharpe Ratio from trades CSV file.')
-parser.add_argument('filename', type=str, help='The path to the trades CSV file')
-
-# Parse the arguments
-args = parser.parse_args()
-
+# Manually set the filename
+filename = 'BTCLV TRADES correct.csv'
 
 # Extend pandas functionality with metrics, etc.
 qs.extend_pandas()
 
-
 # Read the CSV file into a DataFrame
-trades = pd.read_csv(args.filename, parse_dates=['End Time', 'Next Start Time'])
+trades = pd.read_csv(filename, parse_dates=['End Time', 'Next Start Time'])
 
 # Set the 'End Time' as the index
 trades.set_index('End Time', inplace=True)
